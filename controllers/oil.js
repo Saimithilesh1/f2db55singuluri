fd48a10fd4f8fd36108f7449cf5eb3a0d5a18c06
Saimithilesh1 <saimithilesh111@gmail.com>
2022-11-17 13:16:02 -0600

    added and updated

--- controllers/oil.js
+++ controllers/oil.js
@@ -1,8 +1,16 @@
 var oil = require('../models/oil');
-// List of all oils
-exports.oil_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: oil list');
+
+exports.oil_list = async function(req, res) {
+    try{
+        theoils = await oil.find();
+        res.send(theoils);
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }  
 };
+
 // for a specific oil.
 exports.oil_detail = async function(req, res) {
     console.log("detail"  + req.params.id)
@@ -31,24 +39,7 @@ exports.oil_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }  
 };
-// Handle oil delete form on DELETE.
-exports.oil_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: oil delete DELETE ' + req.params.id);
-};
-// Handle oil update form on PUT.
-exports.oil_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: oil update PUT' + req.params.id);
-};
-exports.oil_list = async function(req, res) {
-    try{
-        theoils = await oil.find();
-        res.send(theoils);
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
+
 
 // Handle oil update form on PUT.
 exports.oil_update_put = async function(req, res) {
@@ -82,3 +73,70 @@ exports.oil_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }  
 };
+// Handle oil delete on DELETE.
+
+exports.oil_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await oil.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.oil_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await oil.findById( req.query.id)
+    res.render('oildetail',
+   { title: 'oil Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle building the view for creating a oil.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.oil_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('oilcreate', { title: 'oil Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.oil_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await oil.findById(req.query.id)
+    res.render('oilupdate', { title: 'oil Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   // Handle a delete one view with id from query 
+exports.oil_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await oil.findById(req.query.id) 
+        res.render('oildelete', { title: 'oil Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/oil.js
+++ routes/oil.js
@@ -1,8 +1,21 @@
 var express = require('express');
+const oil_controllers= require('../controllers/oil');
 var router = express.Router();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('oil', { title: 'search results for Oil class' });
 
-});
+// GET request for one oil. 
+router.get('/oil/:id', oil_controllers.oil_detail);
+
+/* GET oil */
+router.get('/', oil_controllers.oil_view_all_Page );
 module.exports = router;
+
+
+/* GET detail oil page */
+router.get('/detail', oil_controllers.oil_view_one_Page);
+router.get('/oil/:id', oil_controllers.oil_detail);
+/* GET create oil page */
+router.get('/create', oil_controllers.oil_create_Page);
+/* GET create update page */
+router.get('/update', oil_controllers.oil_update_Page);
+/* GET delete costume page */ 
+router.get('/delete', oil_controllers.oil_delete_Page); 
