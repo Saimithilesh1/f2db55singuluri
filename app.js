b10adff5fcb3fd8e796eaffa06333db13d3a2830
Saimithilesh1 <saimithilesh111@gmail.com>
2022-11-18 00:30:33 -0600

    added files

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ var mongoose = require('mongoose');
 var mongodb = require('mongodb');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+)); 
 var oil = require("./models/oil");
 require('dotenv').config();
 const connectionString =
@@ -46,7 +62,21 @@ app.use('/oil', oilRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
-
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 // We can seed the collection if needed on server start
 async function recreateDB(){
 
